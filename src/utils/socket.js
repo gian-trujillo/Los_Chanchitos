1821939db0231d0b4c639662acaa1d028bcb2bc4
gian-trujillo <giantrujillo98@gmail.com>
2026-06-11 14:19:40 -0600

    fix api link

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:3000";
+const SOCKET_URL = "https://api.loschanchitos.masdiseno.com/api";
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
